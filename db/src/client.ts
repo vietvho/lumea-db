291178bcee2c067041fe2c0b6bfc551fbb28dea8
viet nguyen <vnbillio@gmail.com>
2026-04-27 12:23:42 +0700

    fix process does not exist

--- db/src/client.ts
+++ db/src/client.ts
@@ -2,7 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const connectionString = process.env["DATABASE_URL"];
+const env = typeof process !== "undefined" ? process.env : {};
+const connectionString = env["DATABASE_URL"];
 
 if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
