fc2fc7145895af901c37e4c37802f6ac39cdb20c
viet nguyen <vnbillio@gmail.com>
2026-05-28 05:21:11 +0700

    update package export version

--- db/src/index.ts
+++ db/src/index.ts
@@ -2,4 +2,7 @@ export { jobStatusEnum, creators, sites, jobs } from "./schema";
 export type { Creator, NewCreator, Site, NewSite, Job, NewJob } from "./schema";
 export { db } from "./client";
 export type { Database } from "./client";
-export { eq, desc } from "drizzle-orm";
+import * as drizzle from "drizzle-orm";
+export const eq = drizzle.eq;
+export const desc = drizzle.desc;
+export const count = drizzle.count;
